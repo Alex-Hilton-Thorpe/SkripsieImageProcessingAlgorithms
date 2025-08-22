9cb351caa9066597634864265d1eca24c5865af4
Alex-Hilton-Thorpe <alexhthorpe@gmail.com>
2025-08-22 15:55:02 +0200

    Adding in the number of Frames into account, and adding in the delta for the transistion factor. Added in text file reading

--- ballTracking.cc
+++ ballTracking.cc
@@ -24,8 +24,11 @@
 #include <limits>
 #include <random>
 
-#include "prlite_genmat.hpp"
+#include "prlite_genmat.hpp" // For Guassian Factors
 #include "sqrtmvg.hpp"
+
+#include <fstream> //Reading from textfiles
+#include <sstream>
 
 // In order to use the built in Inference packages
 // standard library headers
@@ -52,6 +55,88 @@ using namespace emdw;
 //  new target in the same way as this example.
 // ##################################################################
 
+// Function to read ball positions from a text file and store them in a vector of pairs
+std::vector<std::pair<double, double>> readPositions(const std::string &filepath)
+{ // Input: filepath → the path to the text file containing x,y coordinates
+
+  // Create an empty vector to store the positions
+  std::vector<std::pair<double, double>> positions;
+
+  // Open the file at the given path for reading
+  std::ifstream infile(filepath);
+
+  // Check if the file opened successfully
+  if (!infile.is_open())
+  {
+    // If the file could not be opened, print an error message
+    std::cerr << "Error: Could not open file " << filepath << std::endl;
+    // Return the empty vector because we cannot read data
+    return positions;
+  }
+
+  // String to hold each line of the file as we read it
+  std::string line;
+
+  // Read the file line by line
+  while (std::getline(infile, line))
+  {
+    // If the line is empty, skip it and continue to the next line
+    if (line.empty())
+      continue;
+
+    // Create a stringstream from the line so we can split it by commas
+    std::stringstream ss(line);
+
+    // Strings to hold the x and y values as text
+    std::string x_str, y_str;
+
+    // Split the line into x and y using the comma as a delimiter
+    // std::getline(ss, x_str, ',') → reads characters until ',' into x_str
+    // std::getline(ss, y_str) → reads the remaining characters into y_str
+    if (std::getline(ss, x_str, ',') && std::getline(ss, y_str))
+    {
+
+      // Convert x from string to double
+      double x = std::stod(x_str);
+      // Convert y from string to double
+      double y = std::stod(y_str);
+
+      // Add the pair (x, y) to the vector of positions
+      positions.emplace_back(x, y);
+    }
+  }
+
+  // Close the file after reading
+  infile.close();
+
+  // Return the vector containing all positions
+  return positions; // Output: vector of pairs of doubles, each pair representing (x, y) positions
+}
+
+// Function to count the number of rows in a text file
+int countFrames(const std::string &filepath)
+{
+
+  std::ifstream infile(filepath);
+
+  if (!infile.is_open())
+  {
+    std::cerr << "Error: Could not open file " << filepath << std::endl;
+    return 0;
+  }
+
+  std::string line;
+  int frames = 0;
+  while (std::getline(infile, line))
+  {
+    if (!line.empty())
+      frames++; // ignore empty lines
+  }
+
+  infile.close();
+  return frames;
+}
+
 int main(int, char *argv[])
 {
 
@@ -75,28 +160,57 @@ int main(int, char *argv[])
     //*********************************************************
     typedef SqrtMVG SG; // Shorthand for SqrtMVG
 
-    int frames = 2; // Number of framess which are being tracked, fps*video length
+    std::vector<std::string> filePaths = {
+        "/home/alex/devel/emdw/build/alg1.txt",
+        "/home/alex/devel/emdw/build/alg2.txt",
+        "/home/alex/devel/emdw/build/alg3.txt"};
 
-    // Examples
-    vector<double> meansx1 = {230.0, 235.0}; // Mean x positions for IPA1
-    vector<double> meansy1 = {250.0, 260.0}; // Mean y positions for IPA1
+    int frames = 0; // Number of framess which are being tracked, fps*video length
 
-    vector<double> meansx2 = {225.0, 232.0}; // Mean x positions for IPA2
-    vector<double> meansy2 = {256.0, 263.0}; // Mean y positions for IPA2
+    for (int i = 0; i < 3; i++)
+    {
+
+      int temp = countFrames(filePaths[i]);
+
+      if (temp > frames)
+      {
+        frames = temp;
+      }
+    }
+
+    cout << "Number of Frames: " << frames << "\n";
+
+    // calling function which populates posAlg1 with all the x,y values from the textfiles
+    std::vector<std::pair<double, double>> posAlg1 = readPositions(filePaths[0]);
+    std::vector<std::pair<double, double>> posAlg2 = readPositions(filePaths[1]);
+    std::vector<std::pair<double, double>> posAlg3 = readPositions(filePaths[2]);
+
+    std::vector<double> posAlg1x, posAlg1y, posAlg2x, posAlg2y, posAlg3x, posAlg3y; // Creating arrays (vectors) from all the x and y positions
+
+    for (int i = 0; i < frames; i++)
+    {
+      posAlg1x.push_back(posAlg1[i].first);
+      posAlg1y.push_back(posAlg1[i].second);
+      posAlg2x.push_back(posAlg2[i].first);
+      posAlg2y.push_back(posAlg2[i].second);
+      posAlg3x.push_back(posAlg3[i].first);
+      posAlg3y.push_back(posAlg3[i].second);
+    }
 
     prlite::RowMatrix<double> cov_Ipa(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
-    cov_Ipa(0, 0) = cov_Ipa(1, 1) = 25.0;
+    cov_Ipa(0, 0) = cov_Ipa(1, 1) = 100.0;   // sets the confidence I have in the algorihtms, lower the more confident
     cov_Ipa(0, 1) = cov_Ipa(1, 0) = 0.0;
 
     prlite::ColVector<double> mean_trans(4); // mean vector for the transition factor
+    mean_trans[0] = mean_trans[1] = mean_trans[2] = mean_trans[3] = 0;
 
-    // Transition factor: (x_prev, y_prev) -> (x, y)
-    prlite::RowMatrix<double> cov_trans(4, 4);
+        // Transition factor: (x_prev, y_prev) -> (x, y)
+        prlite::RowMatrix<double> cov_trans(4, 4);
     for (int d = 0; d < 4; d++)
     {
       for (int e = 0; e < 4; e++)
         cov_trans(d, e) = 0.0; // zero everything
-      cov_trans(d, d) = 50.0;  // set diagonal
+      cov_trans(d, d) = 15.0;  // set diagonal // sets the confience I have in the model, lower the more confident
     } // This can be adjusted as needed
 
     // Start the for loop to iterate through the frames, However might do 2 forms of inference BU and BP
@@ -109,37 +223,52 @@ int main(int, char *argv[])
     for (int i = 0; i < frames; i++)
     {
 
+      vector<rcptr<Factor>> factors;
+
       unsigned int x = 2 * i;
       unsigned int y = 2 * i + 1;
 
-      //Image Processing Algorithm 1 (IPA1) code
-
       prlite::ColVector<double> mn1(2); // mean vector 1
-      mn1[0] = meansx1[i];
-      mn1[1] = meansy1[i];
-      rcptr<Factor> alg_1(uniqptr<SG>(new SG({x, y}, mn1, cov_Ipa))); // Setting up the Guassian factor for IPA1
+      mn1[0] = posAlg1x[i];
+      mn1[1] = posAlg1y[i];
 
-      //Image Processing Algorithm 2 (IPA2) code
+      bool ignoreAlg1 = (mn1[0] == -1 && mn1[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
 
       prlite::ColVector<double> mn2(2); // Mean vector 2
-      mn2[0] = meansx2[i];
-      mn2[1] = meansy2[i];
-      rcptr<Factor> alg_2(uniqptr<SG>(new SG({x, y}, mn2, cov_Ipa))); // Setting up the Guassian factor for IPA2
+      mn2[0] = posAlg2x[i];
+      mn2[1] = posAlg2y[i];
 
-      vector<rcptr<Factor>> factors;
+      bool ignoreAlg2 = (mn2[0] == -1 && mn2[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
 
-      if (i == 0)
+      prlite::ColVector<double> mn3(2);
+      mn3[0] = posAlg3x[i];
+      mn3[1] = posAlg3y[i];
+
+      bool ignoreAlg3 = (mn3[0] == -1 && mn3[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
+
+      if (!ignoreAlg1)
       {
-        factors = {alg_1, alg_2}; // Create a vector of factors for the first frame
+        rcptr<Factor> alg_1(uniqptr<SG>(new SG({x, y}, mn1, cov_Ipa))); // Setting up the Guassian factor for IPA1
+        factors.push_back(alg_1);
       }
-      else
-      {
 
+      if (!ignoreAlg2)
+      {
+        rcptr<Factor> alg_2(uniqptr<SG>(new SG({x, y}, mn2, cov_Ipa))); // Setting up the Guassian factor for IPA2
+        factors.push_back(alg_2);
+      }
+
+      if (!ignoreAlg3)
+      {
+        rcptr<Factor> alg_3(uniqptr<SG>(new SG({x, y}, mn3, cov_Ipa))); // Setting up the Guassian factor for IPA3
+        factors.push_back(alg_3);
+      }
+
+      if (i != 0)
+      {
         rcptr<Factor> trans(new SqrtMVG({x_prev, y_prev, x, y}, mean_trans, cov_trans)); // Transition factor connecting the previous frame's (x_prev, y_prev) to the current frame's (x, y)
-
-        factors = {alg_1, alg_2, trans}; // Create a vector of factors for the subsequent frames
+        factors.push_back(trans);                                                        // Create a vector of factors for the subsequent frames
       }
-
       ClusterGraph cg(ClusterGraph::LTRIP, factors, obvs); // Create a cluster graph with the factors
 
       map<Idx2z, rcptr<Factor>> msgs; // Calibrate via loopy belief propagation (Shafer–Shenoy)
@@ -153,13 +282,27 @@ int main(int, char *argv[])
 
       cout << "LBP belief mean for frame " << (i + 1) << ": " << qPtr_Sqrt->getMean() << endl;
 
-      auto mean = qPtr_Sqrt->getMean();
-      mean_trans[0] = mean[0]; // expected delta x prev
-      mean_trans[1] = mean[1]; // expected delta y prev
-      mean_trans[2] = mean[0]; // expected delta x (for the second observation)
-      mean_trans[3] = mean[1]; // expected delta y (for the second observation)
-      // These are for the transistion factor, which is used to get the next state based on the previous state
+      // need to calculate the usual delta between each frame to account for the change in speed etc
 
+      auto mean = qPtr_Sqrt->getMean();
+      if (i == 0)
+      {
+        mean_trans[0] = mean[0];
+        mean_trans[1] = mean[1];
+        mean_trans[2] = mean[0];
+        mean_trans[3] = mean[1];
+      }
+      else
+      {
+        double deltaX = mean[0] - mean_trans[0];
+        double deltaY = mean[1] - mean_trans[1];
+
+        mean_trans[0] = mean[0];          // x from the prev frame
+        mean_trans[1] = mean[1];          // y from the prev frame
+        mean_trans[2] = mean[0] + deltaX; // expected position (for the second observation) based on this frame and the last frame
+        mean_trans[3] = mean[1] + deltaY; // expected position (for the second observation) based on this frame and the last frame
+        // These are for the transistion factor, which is used to get the next state based on the previous state
+      }
       x_prev = x; // Update previous x and y for the next iteration
       y_prev = y;
     }
