50fa903cb12568b13abd131caefad8a1a878dd20
Alex-Hilton-Thorpe <alexhthorpe@gmail.com>
2025-09-12 12:16:02 +0200

    Commit just before starting the outlier code, implemented 5 different algorithms propogating together

--- ballTracking.cc
+++ ballTracking.cc
@@ -27,7 +27,7 @@
 #include "prlite_genmat.hpp" // For Guassian Factors
 #include "sqrtmvg.hpp"
 
-#include <fstream> //Reading from textfiles
+#include <fstream> //Reading and writing to and from textfiles
 #include <sstream>
 
 // In order to use the built in Inference packages
@@ -137,12 +137,34 @@ int countFrames(const std::string &filepath)
   return frames;
 }
 
+static void outputToFile(double x, double y)
+{
+
+  std::string outputFile = "/home/alex/devel/emdw/build/outputs.txt";
+
+  // Open file in append mode
+  std::ofstream outfile(outputFile, std::ios::app);
+
+  // Check if file opened
+  if (!outfile.is_open())
+  {
+    std::cerr << "Error: Could not open file " << outputFile << std::endl;
+    return;
+  }
+
+  // Write x and y separated by a comma, then go to the next line
+  outfile << x << "," << y << std::endl;
+}
+
 int main(int, char *argv[])
 {
 
   // NOTE: this activates logging and unit tests
   initLogging(argv[0]);
   prlite::TestCase::runAllTests();
+
+  std::ofstream outfile("/home/alex/devel/emdw/build/outputs.txt", std::ios::trunc);
+  outfile.close();
 
   try
   {
@@ -163,11 +185,13 @@ int main(int, char *argv[])
     std::vector<std::string> filePaths = {
         "/home/alex/devel/emdw/build/alg1.txt",
         "/home/alex/devel/emdw/build/alg2.txt",
-        "/home/alex/devel/emdw/build/alg3.txt"};
+        "/home/alex/devel/emdw/build/alg3.txt",
+        "/home/alex/devel/emdw/build/alg4.txt",
+        "/home/alex/devel/emdw/build/alg5.txt"};
 
     int frames = 0; // Number of framess which are being tracked, fps*video length
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 5; i++)
     {
 
       int temp = countFrames(filePaths[i]);
@@ -184,8 +208,10 @@ int main(int, char *argv[])
     std::vector<std::pair<double, double>> posAlg1 = readPositions(filePaths[0]);
     std::vector<std::pair<double, double>> posAlg2 = readPositions(filePaths[1]);
     std::vector<std::pair<double, double>> posAlg3 = readPositions(filePaths[2]);
+    std::vector<std::pair<double, double>> posAlg4 = readPositions(filePaths[3]);
+    std::vector<std::pair<double, double>> posAlg5 = readPositions(filePaths[4]);
 
-    std::vector<double> posAlg1x, posAlg1y, posAlg2x, posAlg2y, posAlg3x, posAlg3y; // Creating arrays (vectors) from all the x and y positions
+    std::vector<double> posAlg1x, posAlg1y, posAlg2x, posAlg2y, posAlg3x, posAlg3y, posAlg4x, posAlg4y, posAlg5x, posAlg5y; // Creating arrays (vectors) from all the x and y positions
 
     for (int i = 0; i < frames; i++)
     {
@@ -195,22 +221,49 @@ int main(int, char *argv[])
       posAlg2y.push_back(posAlg2[i].second);
       posAlg3x.push_back(posAlg3[i].first);
       posAlg3y.push_back(posAlg3[i].second);
+      posAlg4x.push_back(posAlg4[i].first);
+      posAlg4y.push_back(posAlg4[i].second);
+      posAlg5x.push_back(posAlg5[i].first);
+      posAlg5y.push_back(posAlg5[i].second);
     }
 
-    prlite::RowMatrix<double> cov_Ipa(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
-    cov_Ipa(0, 0) = cov_Ipa(1, 1) = 100.0;   // sets the confidence I have in the algorihtms, lower the more confident
-    cov_Ipa(0, 1) = cov_Ipa(1, 0) = 0.0;
+    // These need to be refined per algorithm
+    prlite::RowMatrix<double> cov_Ipa1(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
+    cov_Ipa1(0, 0) = 0.2;
+    cov_Ipa1(1, 1) = 0.2;     // sets the confidence I have in the algorihtms, lower the more confident
+    cov_Ipa1(0, 1) = cov_Ipa1(1, 0) = 0.0;
+
+    prlite::RowMatrix<double> cov_Ipa2(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
+    cov_Ipa2(0, 0) = 0.2;
+    cov_Ipa2(1, 1) = 0.2;     // sets the confidence I have in the algorihtms, lower the more confident
+    cov_Ipa2(0, 1) = cov_Ipa2(1, 0) = 0.0;
+
+    prlite::RowMatrix<double> cov_Ipa3(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
+    cov_Ipa3(0, 0) = 0.2;
+    cov_Ipa3(1, 1) = 0.2;     // sets the confidence I have in the algorihtms, lower the more confident
+    cov_Ipa3(0, 1) = cov_Ipa3(1, 0) = 0.0;
+
+    prlite::RowMatrix<double> cov_Ipa4(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
+    cov_Ipa4(0, 0) = 0.2;
+    cov_Ipa4(1, 1) = 0.2;     // sets the confidence I have in the algorihtms, lower the more confident
+    cov_Ipa4(0, 1) = cov_Ipa4(1, 0) = 0.0;
+
+    prlite::RowMatrix<double> cov_Ipa5(2, 2); // covariance matrix 1 (I have set these as constant, but may need to modify as I get furhter for more refinement)
+    cov_Ipa5(0, 0) = 0.2;
+    cov_Ipa5(1, 1) = 0.2;     // sets the confidence I have in the algorihtms, lower the more confident
+    cov_Ipa5(0, 1) = cov_Ipa5(1, 0) = 0.0;
 
     prlite::ColVector<double> mean_trans(4); // mean vector for the transition factor
     mean_trans[0] = mean_trans[1] = mean_trans[2] = mean_trans[3] = 0;
 
-        // Transition factor: (x_prev, y_prev) -> (x, y)
-        prlite::RowMatrix<double> cov_trans(4, 4);
+    // These need to be refined
+    //  Transition factor: (x_prev, y_prev) -> (x, y)
+    prlite::RowMatrix<double> cov_trans(4, 4);
     for (int d = 0; d < 4; d++)
     {
       for (int e = 0; e < 4; e++)
         cov_trans(d, e) = 0.0; // zero everything
-      cov_trans(d, d) = 15.0;  // set diagonal // sets the confience I have in the model, lower the more confident
+      cov_trans(d, d) = 0.2;   // set diagonal // sets the confience I have in the model, lower the more confident
     } // This can be adjusted as needed
 
     // Start the for loop to iterate through the frames, However might do 2 forms of inference BU and BP
@@ -240,30 +293,57 @@ int main(int, char *argv[])
 
       bool ignoreAlg2 = (mn2[0] == -1 && mn2[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
 
-      prlite::ColVector<double> mn3(2);
+      prlite::ColVector<double> mn3(2); // Mean vector 3
       mn3[0] = posAlg3x[i];
       mn3[1] = posAlg3y[i];
 
       bool ignoreAlg3 = (mn3[0] == -1 && mn3[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
 
+      prlite::ColVector<double> mn4(2); // Mean vector 4
+      mn4[0] = posAlg4x[i];
+      mn4[1] = posAlg4y[i];
+
+      bool ignoreAlg4 = (mn4[0] == -1 && mn4[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
+
+      prlite::ColVector<double> mn5(2); // Mean vector 5
+      mn5[0] = posAlg5x[i];
+      mn5[1] = posAlg5y[i];
+
+      bool ignoreAlg5 = (mn5[0] == -1 && mn5[1] == -1); // to see if we should ignore this algorithm ie. if x,y = -1,-1;
+
       if (!ignoreAlg1)
       {
-        rcptr<Factor> alg_1(uniqptr<SG>(new SG({x, y}, mn1, cov_Ipa))); // Setting up the Guassian factor for IPA1
+        std::cout << "Frame " << i << " Alg1: (" << mn1[0] << "," << mn1[1] << ") ignore=" << ignoreAlg1 << "\n";
+        rcptr<Factor> alg_1(uniqptr<SG>(new SG({x, y}, mn1, cov_Ipa1))); // Setting up the Guassian factor for IPA1
         factors.push_back(alg_1);
       }
 
       if (!ignoreAlg2)
       {
-        rcptr<Factor> alg_2(uniqptr<SG>(new SG({x, y}, mn2, cov_Ipa))); // Setting up the Guassian factor for IPA2
+        std::cout << "Frame " << i << " Alg2: (" << mn2[0] << "," << mn2[1] << ") ignore=" << ignoreAlg2 << "\n";
+        rcptr<Factor> alg_2(uniqptr<SG>(new SG({x, y}, mn2, cov_Ipa2))); // Setting up the Guassian factor for IPA2
         factors.push_back(alg_2);
       }
 
       if (!ignoreAlg3)
       {
-        rcptr<Factor> alg_3(uniqptr<SG>(new SG({x, y}, mn3, cov_Ipa))); // Setting up the Guassian factor for IPA3
+        std::cout << "Frame " << i << " Alg3: (" << mn3[0] << "," << mn3[1] << ") ignore=" << ignoreAlg3 << "\n";
+        rcptr<Factor> alg_3(uniqptr<SG>(new SG({x, y}, mn3, cov_Ipa3))); // Setting up the Guassian factor for IPA3
         factors.push_back(alg_3);
       }
 
+      if (!ignoreAlg4)
+      {
+        std::cout << "Frame " << i << " Alg4: (" << mn4[0] << "," << mn4[1] << ") ignore=" << ignoreAlg4 << "\n";
+        rcptr<Factor> alg_4(uniqptr<SG>(new SG({x, y}, mn4, cov_Ipa4))); // Setting up the Guassian factor for IPA4
+        factors.push_back(alg_4);
+      }
+      if (!ignoreAlg5)
+      {
+        std::cout << "Frame " << i << " Alg5: (" << mn5[0] << "," << mn5[1] << ") ignore=" << ignoreAlg5 << "\n";
+        rcptr<Factor> alg_5(uniqptr<SG>(new SG({x, y}, mn5, cov_Ipa5))); // Setting up the Guassian factor for IPA5
+        factors.push_back(alg_5);
+      }
       if (i != 0)
       {
         rcptr<Factor> trans(new SqrtMVG({x_prev, y_prev, x, y}, mean_trans, cov_trans)); // Transition factor connecting the previous frame's (x_prev, y_prev) to the current frame's (x, y)
@@ -303,6 +383,8 @@ int main(int, char *argv[])
         mean_trans[3] = mean[1] + deltaY; // expected position (for the second observation) based on this frame and the last frame
         // These are for the transistion factor, which is used to get the next state based on the previous state
       }
+
+      outputToFile(mean[0], mean[1]);
       x_prev = x; // Update previous x and y for the next iteration
       y_prev = y;
     }
